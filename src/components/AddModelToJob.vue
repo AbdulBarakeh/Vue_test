1e0eb3516c9d70cabdedb107925a29d5be48109b
Abdul-Rahman Abdurrohman <abdulbarakeh98@gmail.com>
2021-05-21 20:58:13 +0200

    fix

--- src/components/AddModelToJob.vue
+++ src/components/AddModelToJob.vue
@@ -5,11 +5,11 @@
 
       <form >
         <select v-model="job" name="jobs" id="jobs">
-          <option  v-bind:key="job.efJobId" v-for="job in jobs">{{job.customer}} - {{job.location}}</option></select
+          <option :value="job.efJobId" v-bind:key="job.efJobId" v-for="job in jobs">{{job.customer}} - {{job.location}}</option></select
         ><br />
         <label>Wich model would you like to add?</label><br />
         <select v-model="model" name="models" id="models">
-          <option  v-bind:key="model.efModelId" v-for="model in models">{{model.firstName}} {{model.lastName}}</option></select
+          <option :value="model.efModelId" v-bind:key="model.efModelId" v-for="model in models">{{model.firstName}} {{model.lastName}}</option></select
         ><br />
         <input type="submit" value="Add model to the job" v-on:click="addModel" />
       </form>
@@ -32,6 +32,24 @@ export default {
     addModel(e){
       console.dir(this.job)
       console.dir(this.model)
+
+       let url = `https://localhost:44368/api/Jobs/${this.job}/model/${this.model}`;
+
+       axios
+        .post(url,null ,{headers:{
+          Authorization: "Bearer " + localStorage.getItem("token")
+        }})
+        .then((res) => {
+          this.jobs = res.data;
+          console.log(res.data);
+        })
+        .catch((error) => {
+          if (error.status === 401) {
+            alert("unautherized please login");
+          } else {
+            alert("Something bad happened" + error);
+          }
+        });
       e.preventDefault();
     },
     getjobs() {
