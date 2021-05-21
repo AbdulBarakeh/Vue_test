48182012780c16ea71042dbac43d579a2ae8c07a
Rune Matthias Hell <au550573@uni.au.dk>
2021-05-21 21:45:28 +0200

    begin addexpense, delete createmodel

--- src/components/AddExpenseToJob.vue
+++ src/components/AddExpenseToJob.vue
@@ -1,19 +1,17 @@
 <template>
     <div class="wrapper fadeInDown">
       <div id="formContent">
-        <h3>Add Expense to job</h3>
+        <h3>{{msg}}</h3>
           
         <form>
-          <select name="" id="">
-            <option value="">Choose job to add expense</option>
+          <select  v-model="job" name="jobs" id="jobs">
+            <option :value="job.efJobId" v-bind:key="job.efJobId" v-for="job in jobs">{{job.customer}} - {{job.location}}</option>
           </select><br>
           <label>Put your expense on the job here</label>
-          <input
-          type="Text"
-          />
+          <input type="Text" v-model="Amount"/>
           <label>What is the expense used on?</label>
-          <input type="text">
-          <input type="submit" value="Add Expense">
+          <input type="text" v-model="Text">
+          <input type="submit" value="Add Expense" v-on:click="addExpense">
 
         </form>
       </div>
@@ -23,8 +21,45 @@
 export default {
   data() {
     return {
-      msg: "AddExpenseToJobs",
+      msg: "Add Expense To a Job",
+      jobs:[],
+      Amount: "",
+      Text: "", 
     };
+  },
+  methods: {
+    addExpense(){
+      var form = {
+        Amount: Number(this.Amount),
+        Text: this.Text,
+      }
+    },
+    getjobs() {
+      let url = "https://localhost:44368/api/Jobs";
+      axios
+        .get(url, {
+          method: "GET", // Or DELETE
+          credentials: "include",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        })
+        .then((res) => {
+          this.jobs = res.data;
+          console.log(res.data);
+        })
+        .catch((error) => {
+          if (error.status === 401) {
+            alert("unautherized please login");
+          } else {
+            alert("Something bad happened" + error);
+          }
+        });
+    },
+  },
+  
+  mounted() {
+    this.getjobs();
   },
 };
 </script>
