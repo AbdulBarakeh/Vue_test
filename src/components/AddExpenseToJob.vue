3e7c231cfda259aed13a07d83a27d129b8dd6028
Kasper Fogh Jensen <phogh15@gmail.com>
2021-05-21 23:23:24 +0200

    jwt token to modelID

--- src/components/AddExpenseToJob.vue
+++ src/components/AddExpenseToJob.vue
@@ -124,9 +124,25 @@ export default {
         });
     },
   },
+
+    parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+},
   mounted() {
     this.getjobs();
     this.getmodels();
+
+    var token = localStorage.getItem("token")
+    var modelID = JSON.parse(atob(token.split('.')[1])).ModelId;
+    console.dir(modelID);
+
+
   },
 };
 </script>
