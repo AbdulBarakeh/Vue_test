7f3e7db34fd15962f61bdaa65c6077e7f304a41d
Abdul-Rahman Abdurrohman <abdulbarakeh98@gmail.com>
2021-05-21 21:41:02 +0200

    fix

--- src/components/RemoveModelFromJob.vue
+++ src/components/RemoveModelFromJob.vue
@@ -4,13 +4,12 @@
       <h3>{{ msg }}</h3>
       <form action="">
         <select v-model="job" name="jobs" id="jobs">
-          <option  v-bind:key="job.efJobId" v-for="job in jobs">{{job.customer}} - {{job.location}}</option></select
+          <option :value="job.efJobId" v-bind:key="job.efJobId" v-for="job in jobs">{{job.customer}} - {{job.location}}</option></select
         ><br />
-        <input type="submit" valuue="Select job" v-on:click="selectJob"/>
         <label for="">Wich model would you like to remove?</label><br />
-        <select name="" id="">
-          <option value="">Models assigned to job</option>
-        </select>
+        <select v-model="model" name="models" id="models">
+          <option :value="model.efModelId" v-bind:key="model.efModelId" v-for="model in models">{{model.firstName}} {{model.lastName}}</option></select
+        ><br />
         <input type="submit" value="Remove model from the job" v-on:click="removeModel"/>
       </form>
     </div>
@@ -22,16 +21,42 @@ export default {
   data() {
     return {
       msg: "Remove a model from a job",
-      jobs: []
+      jobs: [],
+      models: [],
+      job:{},
+      model:{}
     };
   },
 
   methods: {
-    removeModel() {
-      let url = "https://localhost:44368/api/Jobs/{jobId}/model/{modelId}";
+
+    removeModel(e) {
+      let url = `https://localhost:44368/api/Jobs/${this.job}/model/${this.model}`;
+      console.log(url);
+      axios
+        .delete(url, {
+          credentials: "include",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        })
+        .then((res) => {
+          this.jobs = res.data;
+          console.log(res.data);
+        })
+        .catch((error) => {
+          if (error.status === 401) {
+            alert("unautherized please login");
+          } else {
+            alert("Something bad happened" + error);
+          }
+        });
+      e.preventDefault();
+    }, getjobs() {
+      let url = "https://localhost:44368/api/Jobs";
       axios
         .get(url, {
-          method: "DELETE",
+          method: "GET", // Or DELETE
           credentials: "include",
           headers: {
             Authorization: "Bearer " + localStorage.getItem("token"),
@@ -49,20 +74,18 @@ export default {
           }
         });
     },
-  },
-
-  mounted(){
-      let url = "https://localhost:44368/api/Jobs";
+    getmodels() {
+      let url = "https://localhost:44368/api/Models";
       axios
         .get(url, {
-          method: "GET", 
+          method: "GET", // Or DELETE
           credentials: "include",
           headers: {
             Authorization: "Bearer " + localStorage.getItem("token"),
           },
         })
         .then((res) => {
-          this.jobs = res.data;
+          this.models = res.data;
           console.log(res.data);
         })
         .catch((error) => {
@@ -72,6 +95,12 @@ export default {
             alert("Something bad happened" + error);
           }
         });
+    },
+  },
+
+  mounted(){
+      this.getjobs();
+      this.getmodels();
     }
 };
 
